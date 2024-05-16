4a1d388edaa83dfb312d644f8cc96b8a57fd5aac
Sulwe <cypriansulwe.cs@gmail.com>
2024-05-16 13:27:27 +0300

    create and connect to database

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-});
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server")
+});
+
+mongoose.connect("mongodb+srv://cypriansulwecs:8cSkRnVWt8JLnx33@cluster0.xsxvbwl.mongodb.net/Simple-Crud-App?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
 })
+.catch(() => {
+    console.error("Connection Failed!");
+});
