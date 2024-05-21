a7d7cffcfda365279584c2d35d7b3abb9cf1dde8
Sulwe <cypriansulwe.cs@gmail.com>
2024-05-21 13:36:38 +0300

    reorganize code into different directories

--- index.js
+++ index.js
@@ -1,61 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Product  = require('./models/product.model.js');
+const productRoute = require('./routes/product.route.js');
 const app = express();
 
+//middleware
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//routes
+
+app.use("/api/products", productRoute);
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server")
 });
 
-app.get('/api/products', async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    }
-    catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
 
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-
-    }
-    catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } 
-    catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
-
-app.put('/api/product/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-
-        if(!product) {
-            return res.status(404).json({message: "Product not found"});
-        }
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-    }
-    catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
 
 mongoose.connect("mongodb+srv://cypriansulwecs:8cSkRnVWt8JLnx33@cluster0.xsxvbwl.mongodb.net/Simple-Crud-App?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => {
